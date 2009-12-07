aec50cb2a68360b43dde135ffd5042f05accbe9b
AndrewGDRowley <AndrewGDRowley@b07844b4-2286-11de-8297-c3254a27cc66>
2009-12-07 09:30:13 +0000

    Fixed bug that stopped the layout being changed

--- ViCoVRE/ViCoGWTRecorderInterface/src/main/java/com/googlecode/vicovre/gwtinterface/client/LayoutPopup.java
+++ ViCoVRE/ViCoGWTRecorderInterface/src/main/java/com/googlecode/vicovre/gwtinterface/client/LayoutPopup.java
@@ -32,13 +32,11 @@
 
 package com.googlecode.vicovre.gwtinterface.client;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -231,7 +229,7 @@ public class LayoutPopup extends ModalPopup<VerticalPanel> implements
             layout.setHeight((maxY + minY) + "px");
         }
         if (isShowing()) {
-            center();
+            super.center();
         }
     }
 
